d1acec07a725ce6425c63747e531e0d2c14e0f61
nerdbetter <jasontlogan@gmail.com>
2017-06-27 11:57:26 -0500

    spell and weapon objects create on submit

--- js/attackBuilder.js
+++ js/attackBuilder.js
@@ -10,8 +10,8 @@ function WeaponAttacks(name, diceType, diceNumber, baseStat, description){
 }
 //submit for weaponAttacks
 var form = document.querySelectorAll('form')[0];
-form.addEventListener('submit', formSubmit);
-function formSubmit(event){
+form.addEventListener('submit', submitAttack);
+function submitAttack(event){
   event.preventDefault();
   var name = document.querySelectorAll('input[name="name"]')[0];
   var diceType = document.querySelectorAll('select[name="diceType"]')[0];
@@ -35,14 +35,14 @@ function SpellAttacks(name, diceType, diceNumber, baseStat, spellLevel, descript
 }
 //submit for spellAttacks
 var form = document.querySelectorAll('form')[1];
-form.addEventListener('submit', formSubmit);
-function formSubmit(event){
+form.addEventListener('submit', submitSpell);
+function submitSpell(event){
   event.preventDefault();
   var name = document.querySelectorAll('input[name="name"]')[1];
   var diceType = document.querySelectorAll('select[name="diceType"]')[1];
   var diceNumber = document.querySelectorAll('select[name="diceNumber"]')[1];
   var baseStat = document.querySelectorAll('select[name="baseStat"]')[1];
-  var baseStat = document.querySelectorAll('select[name="spellLevel"]')[1];
+  var spellLevel = document.querySelectorAll('select[name="spellLevel"]')[0];
   var description =  document.querySelectorAll('input[name="description"]')[1];
 
   var spellAttacks = new SpellAttacks( name.value, diceType.value, diceNumber.value, baseStat.value, spellLevel.value, description.value);
