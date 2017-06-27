d820a16ad1c20321baf95958be5c13643cefaf19
nerdbetter <jasontlogan@gmail.com>
2017-06-27 12:10:16 -0500

    submit creates arrays of attacks

--- js/attackBuilder.js
+++ js/attackBuilder.js
@@ -1,12 +1,12 @@
 //weapon attack!
 var weaponAttacks = [];
-function WeaponAttacks(name, diceType, diceNumber, baseStat, description){
+
+function WeaponAttack(name, diceType, diceNumber, baseStat, description){
   this.name = name;
   this.diceType = diceType;
   this.diceNumber = diceNumber;
   this.baseStat = baseStat;
   this.description = description;
-  weaponAttacks.push(this);
 }
 //submit for weaponAttacks
 var form = document.querySelectorAll('form')[0];
@@ -19,19 +19,21 @@ function submitAttack(event){
   var baseStat = document.querySelectorAll('select[name="baseStat"]')[0];
   var description =  document.querySelectorAll('input[name="description"]')[0];
 
-  var weaponAttacks = new WeaponAttacks( name.value, diceType.value, diceNumber.value, baseStat.value, description.value);
+  var weaponAttack = new WeaponAttack( name.value, diceType.value, diceNumber.value, baseStat.value, description.value);
+
+  weaponAttacks.push(weaponAttack);
+
   console.log(weaponAttacks);
 };
 //spell Attacks!
 var spellAttacks = [];
-function SpellAttacks(name, diceType, diceNumber, baseStat, spellLevel, description){
+function SpellAttack(name, diceType, diceNumber, baseStat, spellLevel, description){
   this.name = name;
   this.diceType = diceType;
   this.diceNumber = diceNumber;
   this.baseStat = baseStat;
   this.spellLevel = spellLevel;
   this.description = description;
-  spellAttacks.push(this);
 }
 //submit for spellAttacks
 var form = document.querySelectorAll('form')[1];
@@ -45,6 +47,9 @@ function submitSpell(event){
   var spellLevel = document.querySelectorAll('select[name="spellLevel"]')[0];
   var description =  document.querySelectorAll('input[name="description"]')[1];
 
-  var spellAttacks = new SpellAttacks( name.value, diceType.value, diceNumber.value, baseStat.value, spellLevel.value, description.value);
+  var spellAttack = new SpellAttack( name.value, diceType.value, diceNumber.value, baseStat.value, spellLevel.value, description.value);
+
+  spellAttacks.push(spellAttack);
+
   console.log(spellAttacks);
 };
