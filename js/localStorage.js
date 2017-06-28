b4732ea302c807a21297e839561fe41d06b67b4e
nerdbetter <jasontlogan@gmail.com>
2017-06-28 13:35:50 -0500

    attackroll works

--- js/localStorage.js
+++ js/localStorage.js
@@ -5,7 +5,7 @@ var attacks;
 window.onload = function() {
   load();
   attackForm();
-}
+};
 function load() {
   if (localStorage.getItem('attacks') != null){
     attacks = JSON.parse(localStorage.getItem('attacks'));
--- js/rolls.js
+++ js/rolls.js
@@ -2,46 +2,64 @@
 console.log('rolls.js loaded');
 
 function abilityScore(baseStat) {
-    var stat = Character[baseStat];
-    if(stat <= 3 ){
-      return -4;
-    }
-    if(stat <= 5 ){
-      return -3;
+  var stat;
+  if(baseStat == 'strength'){
+    stat = characters[0].strength;
+  }
+  else if(baseStat == 'dexterity'){
+    stat = characters[0].dexterity;
+  }
+  else if(baseStat == 'constitution'){
+    stat = characters[0].constitution;
+  }
+  else if(baseStat == 'intelligence'){
+    stat = characters[0].intelligence;
+  }
+  else if(baseStat == 'wisdom'){
+    stat = characters[0].wisdom;
+  }
+  else if(baseStat == 'charisma'){
+    stat = characters[0].charisma;
+  }
+  if(stat <= 3 ){
+    return -4;
+  }
+  if(stat <= 5 ){
+    return -3;
 
-    }
-    if(stat <= 7 ){
-      return -2;
+  }
+  if(stat <= 7 ){
+    return -2;
 
-    }
-    if(stat <= 9 ){
-      return -1;
+  }
+  if(stat <= 9 ){
+    return -1;
 
-    }
-    if(stat <= 11 ){
-      return 0;
+  }
+  if(stat <= 11 ){
+    return 0;
 
-    }
-    if(stat <= 13 ){
-      return 1;
+  }
+  if(stat <= 13 ){
+    return 1;
 
-    }
-    if(stat <= 15 ){
-      return 2;
+  }
+  if(stat <= 15 ){
+    return 2;
 
-    }
-    if(stat <= 17){
-      return 3;
+  }
+  if(stat <= 17){
+    return 3;
 
-    }
-    if(stat <= 19 ){
-      return 4;
+  }
+  if(stat <= 19 ){
+    return 4;
 
-    }
-    if(stat <= 21 ){
-      return 5;
+  }
+  if(stat <= 21 ){
+    return 5;
 
-    }
+  }
 }
 
 function diceRoll(diceType, diceNumber){
