b5fcea84f63bfcf67cdfda4500dff6b496457df8
Meepedy <david@psyon.org>
2017-06-29 22:17:59 -0500

    format cleanup attackForm.js

--- js/attackForm.js
+++ js/attackForm.js
@@ -1,16 +1,15 @@
-
 function attackForm() {
 
-console.log(characters[selectedCharacter].attacks);
-var select = document.getElementById('attacks');
-while (select.firstChild) {
+  console.log(characters[selectedCharacter].attacks);
+  var select = document.getElementById('attacks');
+  while (select.firstChild) {
     select.removeChild(select.firstChild);
-}
-for(var i = 0; i < characters[selectedCharacter].attacks.length; i++) {
-  var attack = characters[selectedCharacter].attacks[i];
-  var el = document.createElement('option');
-  el.textContent = attack.name;
-  el.value = i;
-  select.appendChild(el);
+  }
+  for (var i = 0; i < characters[selectedCharacter].attacks.length; i++) {
+    var attack = characters[selectedCharacter].attacks[i];
+    var el = document.createElement('option');
+    el.textContent = attack.name;
+    el.value = i;
+    select.appendChild(el);
   };
 }
